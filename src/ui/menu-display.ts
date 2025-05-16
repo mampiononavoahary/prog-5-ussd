a98e5eb2eecef6883805951caec9ad5a940a21e7
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-05-16 09:49:00 +0300

    chore: add a new file for the display menu

---
+++ src/ui/menu-display.ts
@@ -0,0 +1,25 @@
+
+export function displayMainMenu() {
+  console.log("\n--- MVOLA ---");
+  console.log("1. Acheter Credit ou Offre Yas");
+  console.log("2. Transferer argent(vers toute destination)");
+  console.log("3. Mvola Credit ou Epargne");
+  console.log("4. Retrait d'argent");
+  console.log("5. Paiment Factures & Partenaires");
+  console.log("6. Mon compte");
+  console.log("7. Recevoir de l'argent");
+  console.log("8. Banques et Micro-Finances");
+  console.log("0. Quitter");
+}
+
+export function displayRechargeOptions() {
+  console.log("1. Recharger directement");
+  console.log("2. Code de recharge");
+}
+
+export function displayCreditBundleOptions() {
+  console.log("1. Credit pour mon numero");
+  console.log("2. Credit pour autre numero");
+  console.log("3. Offre pour mon numero");
+  console.log("4. Offre pour autre numero");
+}
